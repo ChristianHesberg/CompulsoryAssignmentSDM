7aa4d8d431733c1366b6fb0bd11a365e98b641f5
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-09-27 14:14:24 +0200

    Refactoring all methods

--- CompulsoryPractice/Core/Service/ReviewService.cs
+++ CompulsoryPractice/Core/Service/ReviewService.cs
@@ -17,96 +17,68 @@ public class ReviewService : IReviewService
     // 1. On input N, what are the number of reviews from reviewer N?
     public int GetNumberOfReviewsFromReviewer(int reviewer)
     {
-        int count = 0;
-        foreach (BEReview review in _reviewRepository.GetAllReviews())
-        {
-            if (review.Reviewer == reviewer)
-                count++;
-        }
-        return count;
+        BEReview[] allReviews = _reviewRepository.GetAllReviews();
+        return allReviews
+            .Count(review => review.Reviewer == reviewer);
     }
     
     // 2. On input N, what is the average rate that reviewer N had given?
     public double GetAverageRateFromReviewer(int reviewer)
     {
-        List<BEReview> reviewList = _reviewRepository.GetAllReviews().ToList();
-        if (!reviewList.Select(review => review.Reviewer).Contains(reviewer))
-        {
-            throw new ArgumentException("Reviewer does not exist");
-        }
-        return reviewList
-                .Where(r => r.Reviewer == reviewer)
+        BEReview[] allReviews = _reviewRepository.GetAllReviews();
+        if (allReviews.Any(review => review.Reviewer == reviewer))
+            return allReviews.Where(review => review.Reviewer == reviewer)
                 .Select(r => r.Grade)
                 .Average();
+        
+        throw new ArgumentException("Reviewer does not exist");
     }
 
     // 3. On input N and R, how many times has reviewer N given rate R?
     public int GetNumberOfRatesByReviewer(int reviewer, int rate)
     {
         if (reviewer <= 0)
-        {
             throw new ArgumentException( "Id of reviewer is not valid");
-        }
 
-        if (rate < 1 || rate > 5)
-        {
+        if (rate is < 1 or > 5)
             throw new ArgumentException("Invalid value of rating");
-        }
 
-        int count = 0;
-        foreach (BEReview review in _reviewRepository.GetAllReviews())
-        {
-            if (review.Reviewer == reviewer && review.Grade == rate)
-                count++;
-        }
-        return count;
+        return _reviewRepository.GetAllReviews()
+            .Count(review => review.Reviewer == reviewer && review.Grade == rate);
     }
     
     // 4. On input N, how many have reviewed movie N?
     public int GetNumberOfReviews(int movie)
     {
-        List<BEReview> reviews = _reviewRepository.GetAllReviews().ToList();
-        if (!reviews.Select(r => r.Movie).Contains(movie))
-            throw new ArgumentException("Movie does not exist");
-        return reviews.Count(r => r.Movie == movie);
+        BEReview[] allReviews = _reviewRepository.GetAllReviews();
+        if (allReviews.Any(review => review.Movie == movie))
+            return allReviews.Count(review => review.Movie == movie);
+        
+        throw new ArgumentException("Movie does not exist");
     }
 
     // 5. On input N, what is the average rate the movie N had received?
     public double GetAverageRateOfMovie(int movie)
     {
         if (movie <= 0)
-        {
             throw new ArgumentException("Invalid movie ID");
-        }
-
-        int total = 0;
-        int count = 0;
-        foreach (BEReview review in _reviewRepository.GetAllReviews())
-        {
-            if (review.Movie == movie)
-            {
-                total += review.Grade;
-                count++;
-            }
-        }
-        return (double) total/count;
+        
+        BEReview[] allReviews = _reviewRepository.GetAllReviews();
+        return allReviews.Where(review => review.Movie == movie)
+            .Average(review => review.Grade);
     }
 
     // 6. On input N and R, how many times had movie N received rate R?
     public int GetNumberOfRates(int movie, int rate)
     {
-        if (!(1 <= rate && rate <= 5))
-        {
+        if (rate is < 1 or > 5)
             throw new ArgumentException("Invalid value for rate");
-        }
-        List<BEReview> reviewList = _reviewRepository.GetAllReviews().ToList();
-        if (!reviewList.Select(review => review.Movie).Contains(movie))
-        {
-            throw new ArgumentException("Movie does not exist");
-        }
-        return reviewList
-            .Where(review => review.Movie == movie)
-            .Count(review => review.Grade == rate);
+        
+        BEReview[] allReviews = _reviewRepository.GetAllReviews();
+        if (allReviews.Any(review => review.Movie == movie))
+            return allReviews.Count(review => review.Movie == movie && review.Grade == rate);
+        
+        throw new ArgumentException("Movie does not exist");
     }
 
     // 7. What is the id(s) of the movie(s) with the highest number of top rates (5)?
@@ -118,7 +90,7 @@ public class ReviewService : IReviewService
         //  1    ,      1
         //  2    ,      2
         //  3    ,      2
-        Dictionary<int, int> dictionary = _reviewRepository.GetAllReviews().Select(r => r)
+        Dictionary<int, int> dictionary = _reviewRepository.GetAllReviews()
             .Where(r => r.Grade == 5)
             .GroupBy(review => review.Movie)
             .ToDictionary(reviews => reviews.Key, reviews => reviews.Count());
@@ -136,9 +108,9 @@ public class ReviewService : IReviewService
         Dictionary<int, int> dictionary = _reviewRepository.GetAllReviews()
             .GroupBy(review => review.Reviewer)
             .ToDictionary(reviews => reviews.Key, reviews => reviews.Count());
+        
         int maxValue = dictionary.Max(pair => pair.Value);
         return dictionary.Where(pair => pair.Value == maxValue).Select(pair => pair.Key).ToList();
-
     }
 
     // 9. On input N, what is top N of movies? The score of a movie is its average rate.
@@ -172,26 +144,13 @@ public class ReviewService : IReviewService
     {
         BEReview[] allReviews = _reviewRepository.GetAllReviews();
         if (reviewer <= 0 || reviewer > allReviews.Length)
-        {
             throw new ArgumentException("Invalid reviewer ID");
-        }
 
-        return allReviews.
-            Where(r => r.Reviewer == reviewer).
-            OrderByDescending(r => r.ReviewDate)
+        return allReviews
+            .Where(r => r.Reviewer == reviewer)
+            .OrderByDescending(r => r.ReviewDate)
             .ThenBy(r => r.Grade)
             .Select(r => r.Movie).ToList();
-        
-        //listOfReviewedMovie.Where(r => r.Reviewer == reviewer);
-        // var ordered = listOfReviewedMovie.OrderByDescending(r => r.ReviewDate).ThenBy(r => r.Grade);
-        // //var orderedEnumerable = ordered.OrderByDescending(r => r.ReviewDate);
-        // List<int> sortedList = new List<int>();
-        // foreach (var review in ordered)
-        // {
-        //     sortedList.Add(review.Movie);
-        // }
-        //
-        // return sortedList;
     }
 
     // 11. On input N, who are the reviewers that have reviewed movie N? The list
